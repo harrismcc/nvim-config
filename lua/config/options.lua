92d6b0eeacbfa0d704e887bc48a327c25cba45bc
harrismcc <harris@transcend.io>
2023-09-01 09:52:59 -0700

    Update config
    - Removed gh.nvim
    - Added octo for GitHub
    - added distant
    - turned off relative line numbering

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Adds the file path to the top of the vim window
-vim.opt.winbar = "%=%m %f"
+local opt = vim.opt
+
+-- Turn off relative line numbering
+opt.relativenumber = false
+opt.number = true
