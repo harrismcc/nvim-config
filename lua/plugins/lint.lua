825bed68ca58f7ab25adf2be597b07f5996997ce
harrismcc <git@harrismccullers.com>
2026-04-27 10:34:48 -0700

    Checkpoint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -39,46 +39,12 @@ return {
         return {}
       end
 
+      local js_filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' }
+
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
         dockerfile = { 'hadolint' },
-        javascript = get_js_linters,
-        javascriptreact = get_js_linters,
-        typescript = get_js_linters,
-        typescriptreact = get_js_linters,
       }
-
-      -- To allow other plugins to add linters to require('lint').linters_by_ft,
-      -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      --
-      -- However, note that this will enable a set of default linters,
-      -- which will cause errors unless these tools are available:
-      -- {
-      --   clojure = { "clj-kondo" },
-      --   dockerfile = { "hadolint" },
-      --   inko = { "inko" },
-      --   janet = { "janet" },
-      --   json = { "jsonlint" },
-      --   markdown = { "vale" },
-      --   rst = { "vale" },
-      --   ruby = { "ruby" },
-      --   terraform = { "tflint" },
-      --   text = { "vale" }
-      -- }
-      --
-      -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
@@ -89,7 +55,15 @@ return {
           -- Only run the linter in buffers that you can modify in order to
           -- avoid superfluous noise, notably within the handy LSP pop-ups that
           -- describe the hovered symbol using Markdown.
-          if vim.opt_local.modifiable:get() then
+          if not vim.opt_local.modifiable:get() then
+            return
+          end
+
+          local ft = vim.bo.filetype
+          if vim.tbl_contains(js_filetypes, ft) then
+            -- Dynamically determine JS/TS linter based on project config
+            lint.try_lint(get_js_linters())
+          else
             lint.try_lint()
           end
         end,
